64e3d9b24991f9df1b8044e4422e2bb5d51d1126
Eduardo Bee <eduardoviniciusbee@gmail.com>
2025-06-11 19:59:51 -0300

    feat: integrando com api

--- pages/login.vue
+++ pages/login.vue
@@ -9,24 +9,24 @@
             <v-window v-model="window">
               <v-window-item value="1" class="justify-center">
                 <v-col>
-                  <v-text-field v-model="name" label="Nome" prepend-icon="mdi-account" />
+                  <v-text-field v-model="nome" label="Nome" prepend-icon="mdi-account" />
                   <v-text-field v-model="email" label="Email" prepend-icon="mdi-email" />
-                  <v-text-field v-model="password" label="Senha" prepend-icon="mdi-lock" :type="show1 ? 'password' : 'text'" :append-inner-icon="show1 ? 'mdi-eye-off' : 'mdi-eye'" @click:append-inner="show1 = !show1" />
-                  <v-text-field v-model="confirmPassword" label="Confirmar Senha" prepend-icon="mdi-lock-check" :type="show2 ? 'password' : 'text'" :append-inner-icon="show2 ? 'mdi-eye-off' : 'mdi-eye'" @click:append-inner="show2 = !show2" />
-                  <v-text-field v-model="cryptokey" label="Chave Crypto" prepend-icon="mdi-key" />
+                  <v-text-field v-model="senha" label="Senha" prepend-icon="mdi-lock" :type="show1 ? 'password' : 'text'" :append-inner-icon="show1 ? 'mdi-eye-off' : 'mdi-eye'" @click:append-inner="show1 = !show1" />
+                  <v-text-field v-model="confirmsenha" label="Confirmar Senha" prepend-icon="mdi-lock-check" :type="show2 ? 'password' : 'text'" :append-inner-icon="show2 ? 'mdi-eye-off' : 'mdi-eye'" @click:append-inner="show2 = !show2" />
+                  <v-text-field v-model="chaveCrypto" label="Chave Crypto" prepend-icon="mdi-key" />
                 </v-col>
               </v-window-item>
               <v-window-item value="2" class="justify-center">
                 <v-col>
                   <v-text-field v-model="email" label="Email" prepend-icon="mdi-email" />
-                  <v-text-field v-model="password" label="Senha" prepend-icon="mdi-lock" :type="show1 ? 'password' : 'text'" :append-inner-icon="show1 ? 'mdi-eye-off' : 'mdi-eye'" @click:append-inner="show1 = !show1" />
+                  <v-text-field v-model="senha" label="Senha" prepend-icon="mdi-lock" :type="show1 ? 'password' : 'text'" :append-inner-icon="show1 ? 'mdi-eye-off' : 'mdi-eye'" @click:append-inner="show1 = !show1" />
                 </v-col>
               </v-window-item>
             </v-window>
           </v-card-text>
           <v-card-actions>
             <v-col class="align-center d-flex flex-column ga-5" >
-              <v-btn @click="validaLogin" variant="outlined" color="red" class="font-weight-bold w-25" >
+              <v-btn @click="login ? validaLogin() : criaConta()" variant="outlined" color="red" class="font-weight-bold w-25" >
                 <span v-if="!loading">{{ !login ? 'Criar Conta' : 'Entrar'  }}</span>
                 <template v-if="loading">
                   <v-progress-circular indeterminate color="blue" size="20" />
@@ -47,30 +47,49 @@ definePageMeta({
 });
 
 export default {
-  name: 'login',
+  nome: 'login',
   data() {
     return {
       window: 0,
       loading: false,
       login: false,
       email: null,
-      name: null,
-      password: null,
-      confirmPassword: null,
+      nome: null,
+      senha: null,
+      confirmsenha: null,
       show1: false,
       show2: false,
-      cryptokey: null,
+      chaveCrypto: null,
       role: null,
       roles: ['funcionario', 'colaborador'],
     };
   },
 
   methods: {
+    async criaConta() {
+      try {
+        const req = {
+          nome: this.nome,
+          email: this.email,
+          senha: this.senha,
+          chaveCrypto: this.chaveCrypto,
+        };
+        const response = await this.$api.post('/contas/post', req);
+        console.log(response.data);
+        if(response.data.type === 'success') {
+          this.$router.push('organizacoes');
+        }
+        
+      } catch (error) {
+        console.log(error);
+      }
+    },
+
     validaLogin() {
       if(this.login) {
         this.loading=true
-        if(this.email==='teste@gmail.com' && this.password==='1234') {
-          this.$router.push({name:'perfil'}) //mudar para tela de dashboard quando a mesma existir
+        if(this.email==='teste@gmail.com' && this.senha==='1234') {
+          this.$router.push({nome:'perfil'}) //mudar para tela de dashboard quando a mesma existir
           return
         };
 
